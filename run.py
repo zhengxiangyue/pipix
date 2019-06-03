93ffbc359f74c7c10c2f10ef0be962d9de0160ee
zhengxiangyue <zhengxiangyue@yahoo.com>
2019-06-03 04:09:53 -0400

    add readme

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 import sys
 import re
 import requests 
-import urllib.request
-
-# get item id through cmd
 
 share_base = 'https://h5.pipix.com/s/'
 json_url = 'https://h5.pipix.com/bds/webapi/item/detail/'
@@ -23,9 +20,15 @@ try:
     item_id = url_search[0]
 except:
     print("Short code is not valid")
+    exit
 
 json_object_request = requests.get(url = json_url, params = {'item_id': item_id}) 
 
 video_url = json_object_request.json()['data']['item']['origin_video_download']['url_list'][0]['url']
-urllib.request.urlretrieve(video_url, item_id + '.mp4') 
 
+# URL of the image to be downloaded is defined as image_url 
+r = requests.get(video_url) # create HTTP response object 
+  
+
+with open(item_id + '.mp4','wb') as f: 
+    f.write(r.content) 
