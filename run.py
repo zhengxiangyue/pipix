040c6b71aae8a4d739c3234d4f72ca5c6774b184
zhengxiangyue <zhengxiangyue@yahoo.com>
2019-06-03 04:11:12 -0400

    add readme

--- run.py
+++ run.py
@@ -6,15 +6,11 @@ share_base = 'https://h5.pipix.com/s/'
 json_url = 'https://h5.pipix.com/bds/webapi/item/detail/'
 
 share_id = sys.argv[1]
-
 share_url = share_base + share_id
-
 json_object_request = requests.get(url = share_url)
-
 item_url = json_object_request.url
 
 url_pattern = r'https://h5.pipix.com/item/(.*)\?.*'
-
 url_search = re.findall(url_pattern, item_url)
 try:
     item_id = url_search[0]
@@ -23,12 +19,8 @@ except:
     exit
 
 json_object_request = requests.get(url = json_url, params = {'item_id': item_id}) 
-
 video_url = json_object_request.json()['data']['item']['origin_video_download']['url_list'][0]['url']
-
-# URL of the image to be downloaded is defined as image_url 
-r = requests.get(video_url) # create HTTP response object 
-  
+r = requests.get(video_url)  
 
 with open(item_id + '.mp4','wb') as f: 
     f.write(r.content) 
